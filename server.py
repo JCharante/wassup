b9de0f91e0fcfa817686bf7ec18ac92350584956
JCharante <git@jcharante.com>
2017-06-14 18:38:07 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def api_root():
+	return 'wassaup man'
+
+
+@app.route('/<text>')
+def api_other(text):
+	return f"Wassup Man. There's no route at /{text}"
+
+
+@app.route('/person/<_id>')
+def api_person(_id):
+	return f"I don't have any information on the person with id {_id}"
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8001, threaded=True)
